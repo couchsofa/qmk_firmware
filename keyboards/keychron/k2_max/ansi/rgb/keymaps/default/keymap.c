bed6556ad35c87ab4b713888d1bc435d1126704e
Arne Rick <arnebastianrick@gmail.com>
2026-01-05 16:41:13 +0100

    Add layers and custom keycodes

--- keyboards/keychron/k2_max/ansi/rgb/keymaps/default/keymap.c
+++ keyboards/keychron/k2_max/ansi/rgb/keymaps/default/keymap.c
@@ -17,26 +17,32 @@
 #include QMK_KEYBOARD_H
 #include "keychron_common.h"
 
+enum custom_keycodes {
+    LANG_DE = SAFE_RANGE,
+    LANG_GR,
+    SZ,
+    CAP_SZ,
+    EURO,
+};
+
 enum layers {
-    L_BASE_EN,
+    BASE,
+    FN,
     L_UMLAUT,
-    L_UMLAUT_CAP,
     L_GREEK,
-    L_GREEK_CAP,
-    L_FUNC,
 };
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[L_BASE_EN] = LAYOUT_ansi_84(
+[BASE] = LAYOUT_ansi_84(
      KC_ESC,   KC_BRID,  KC_BRIU,  KC_MCTRL, KC_LNPAD, RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_SNAP,  KC_DEL,   RGB_MOD,
      KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGUP,
      KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
-     KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_HOME,
-     KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,  KC_UP,    KC_END,
-     KC_LCTL,  KC_LOPTN, KC_LCMMD,                               KC_SPC,                                 KC_RCMMD,MO(L_UMLAUT),KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+     LANG_DE,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_HOME,
+     KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            LANG_GR,  KC_UP,    KC_END,
+     KC_LCTL,  KC_LOPTN, KC_LCMMD,                               KC_SPC,                                 KC_RALT,  MO(FN),   KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
-[L_UMLAUT] = LAYOUT_ansi_84(
+[FN] = LAYOUT_ansi_84(
      _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,  _______,  RGB_TOG,
      _______,  BT_HST1,  BT_HST2,  BT_HST3,  P2P4G,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
      RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
@@ -44,43 +50,105 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      _______,            _______,  _______,  _______,  _______,  BAT_LVL,  NK_TOGG,  _______,  _______,  _______,  _______,            _______,  _______,  _______,
      _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______,  _______),
 
-[L_UMLAUT_CAP] = LAYOUT_ansi_84(
-     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_DEL,   RGB_MOD,
-     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGUP,
-     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
-     KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_HOME,
-     KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,  KC_UP,    KC_END,
-     KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT, MO(L_BASE_EN),KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+[L_UMLAUT] = LAYOUT_ansi_84(
+     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+     _______,  _______,  _______,  EURO,     _______,  _______,  _______,  KC_LBRC,  _______,  KC_SCLN,  _______,  _______,  _______,  _______,            _______,
+     _______,  KC_QUOT,  SZ,       CAP_SZ,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,            _______,
+     _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,  _______,
+     _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______,  _______),
 
 [L_GREEK] = LAYOUT_ansi_84(
-     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_DEL,   RGB_MOD,
-     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGUP,
-     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
-     KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_HOME,
-     KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,  KC_UP,    KC_END,
-     KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT, MO(L_BASE_EN),KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
-
-[L_GREEK_CAP] = LAYOUT_ansi_84(
-     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_DEL,   RGB_MOD,
-     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGUP,
-     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
-     KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_HOME,
-     KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,  KC_UP,    KC_END,
-     KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT, MO(L_BASE_EN),KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
-
-[L_FUNC] = LAYOUT_ansi_84(
-     _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,  _______,  RGB_TOG,
-     _______,  BT_HST1,  BT_HST2,  BT_HST3,  P2P4G,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
-     RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
-     _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,            _______,            _______,
-     _______,            _______,  _______,  _______,  _______,  BAT_LVL,  NK_TOGG,  _______,  _______,  _______,  _______,            _______,  _______,  _______,
+     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+     _______,  _______,  KC_V,     _______,  _______,  _______,  KC_G,     KC_Y,     _______,  KC_U,     _______,  _______,  _______,  _______,            _______,
+     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,            _______,
+     _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,  _______,
      _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______,  _______)
 };
 
+/* Setup windows for language switching:
+ * Setttings -> Time & langaue -> Typing -> Advanced -> Input language hotkeys
+ * EN LAlt + Shift + 0
+ * DE LAlt + Shift + 1
+ * GR LAlt + Shift + 2
+ *
+ * English should be the default language matching the keyboard layout.
+ */
+
 // clang-format on
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+
+    case LANG_DE:
+        if (record->event.pressed) { // press
+
+            // Switch language to DE, activate Umlaut-layer
+            SEND_STRING(SS_LALT(SS_LSFT("1")));
+            layer_on(L_UMLAUT);
+
+        } else { // release
+
+            // Reset language to EN, deactivate Umlaut-layer
+            SEND_STRING(SS_LALT(SS_LSFT("0")));
+            layer_off(L_UMLAUT);
+        }
+        break;
+
+     case LANG_GR:
+        if (record->event.pressed) { // press
+
+            // Switch language to GR, activate Umlaut-layer
+            SEND_STRING(SS_LALT(SS_LSFT("2")));
+            layer_on(L_GREEK);
+
+        } else { // release
+
+            // Reset language to EN, deactivate Umlaut-layer
+            SEND_STRING(SS_LALT(SS_LSFT("0")));
+            layer_off(L_GREEK);
+        }
+        break;
+
+     case SZ:
+        if (record->event.pressed) {
+
+            SEND_STRING("-");
+            //layer_off(L_UMLAUT);
+
+        }
+        break;
+
+      case CAP_SZ:
+        if (record->event.pressed) {
+
+            SEND_STRING(SS_RALT(SS_LSFT("-")));
+            //layer_off(L_UMLAUT);
+
+        }
+        break;
+
+     case EURO:
+        if (record->event.pressed) {
+
+            SEND_STRING(SS_RALT("e"));
+            //layer_off(L_UMLAUT);
+
+        }
+        break;
+    }
+
     if (!process_record_keychron_common(keycode, record)) {
         return false;
     }
     return true;
 }
+/*
+// Key overrides for the german capital esszett
+const key_override_t capital_esszett = ko_make_with_layers(MOD_MASK_SHIFT, KC_S, CAP_SZ, 1 << L_UMLAUT);
+
+const key_override_t **key_overrides = (const key_override_t *[]){
+     &capital_esszett,
+     NULL
+};
+*/
--- keyboards/keychron/k2_max/rules.mk
+++ keyboards/keychron/k2_max/rules.mk
@@ -2,3 +2,5 @@ include keyboards/keychron/common/wireless/wireless.mk
 include keyboards/keychron/common/keychron_common.mk
 
 VPATH += $(TOP_DIR)/keyboards/keychron
+
+KEY_OVERRIDE_ENABLE = yes
